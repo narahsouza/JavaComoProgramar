3c41d03aaeccc57ad7c050d2b2a5b3a2c9e7be35
narahsouza <narahnsa@gmail.com>
2022-07-26 12:04:13 -0300

    Add files

---
+++ exercicios/Ex02_08.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Ex02_08 {
+
+    public static void main (String[] args) {
+
+        int a;
+        int b;
+        int c;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        b = input.nextInt();
+
+        System.out.print("Enter an integer: ");
+        c = input.nextInt();
+
+        a = b * c;
+
+        System.out.printf("Product is %d%n", a);
+    }
+}
---
+++ exercicios/Ex02_14.java
@@ -0,0 +1,15 @@
+public class Ex02_14 {
+
+    public static void main (String[] args) {
+
+        System.out.println(1 + " " + 2 + " " + 3 + " " + 4 + " ");
+
+        System.out.printf("%d %d %d %d %n", 1, 2, 3, 4);
+
+        System.out.print(1 + " ");
+        System.out.print(2 + " ");
+        System.out.print(3 + " ");
+        System.out.print(4);
+
+    }
+}
