bf65f7b3068f521cd571f581a29235d44d92e063
narahsouza <narahnsa@gmail.com>
2022-08-01 22:29:43 -0300

    Update

---
+++ exercicios/Ex02_18.java
@@ -0,0 +1,19 @@
+// Exercício 2.18: Ex02_18.java
+// Exibindo formas com asteriscos
+
+public class Ex02_18 {
+
+    public static void main (String[] args) {
+        
+        System.out.println();
+        System.out.println("*********     ***       *        *");
+        System.out.println("*       *   *     *    ***      * *");
+        System.out.println("*       *  *       *  *****    *   *");
+        System.out.println("*       *  *       *    *     *     *");
+        System.out.println("*       *  *       *    *    *       *");
+        System.out.println("*       *  *       *    *     *     *");
+        System.out.println("*       *  *       *    *      *   *");
+        System.out.println("*       *    *    *     *       * *");
+        System.out.println("*********     ***       *        *");
+    }
+}
