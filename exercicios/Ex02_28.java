acbb40d89273309c771d04cc545263b243476448
Narah Souza <narahnsa@gmail.com>
2022-08-14 14:29:35 -0300

    Update

--- exercicios/Ex02_28.java
+++ exercicios/Ex02_28.java
@@ -1,2 +1,28 @@
 // Exercício 2.28: Ex02_28.java
 // Diâmetro, circunferência e área de um círculo
+
+import java.util.Scanner;
+
+public class Ex02_28 {
+
+    public static void main (String[] args) {
+
+        int raio;
+        double diametro;
+        double circunferencia;
+        double area;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Digite o raio de um círculo como um inteiro: ");
+        raio = input.nextInt();
+
+        diametro = 2 * raio;
+        circunferencia = 2 * Math.PI * raio;
+        area = Math.PI * raio * raio;
+
+        System.out.printf("Diametro = %f%n", diametro);
+        System.out.printf("Circunferencia = %f%n", circunferencia);
+        System.out.printf("Area = %f%n", area);
+    }
+}
---
+++ exercicios/Ex02_29.java
@@ -0,0 +1,2 @@
+// Exercício 2.28: Ex02_28.java
+// O valor inteiro de um caractere
