ccdf885c63d62d8e9dcda9beff793f3badfc628e
narahsouza <narahnsa@gmail.com>
2022-08-18 11:16:07 -0300

    Update

--- exercicios/Ex02_33.java
+++ exercicios/Ex02_33.java
@@ -19,7 +19,7 @@ public class Ex02_33 {
         System.out.print("Insira a sua altura em centimetros: ");
         altura = input.nextInt();
 
-        System.out.printf("Seu IMC: %f%n", peso / altura / altura * 10000);
+        System.out.printf("Seu IMC: %d%n", peso * 10000 / altura / altura);
 
         System.out.printf("%nBMI VALUES %nUnderweight: less than 18.5 %nNormal: between 18.5 and 24.9 %nOverweight: between 25 and 29.9 %nObese: 30 or greater");
 
--- exercicios/Ex02_34.java
+++ exercicios/Ex02_34.java
@@ -5,20 +5,20 @@ public class Ex02_34 {
     
     public static void main (String[] args) {
         
-        double populacaoMundial = 7753;
-        double taxaCrescimentoAnual = 0011 * populacaoMundial;
+        int populacaoMundial = 7753;
+        int taxaCrescimentoAnual = 0011 * populacaoMundial;
 
-        double ano1 = taxaCrescimentoAnual + populacaoMundial;
-        double ano2 = taxaCrescimentoAnual * 2 + populacaoMundial;
-        double ano3 = taxaCrescimentoAnual * 3 + populacaoMundial;
-        double ano4 = taxaCrescimentoAnual * 4 + populacaoMundial;
-        double ano5 = taxaCrescimentoAnual * 5 + populacaoMundial;
+        int ano1 = taxaCrescimentoAnual + populacaoMundial;
+        int ano2 = taxaCrescimentoAnual * 2 + populacaoMundial;
+        int ano3 = taxaCrescimentoAnual * 3 + populacaoMundial;
+        int ano4 = taxaCrescimentoAnual * 4 + populacaoMundial;
+        int ano5 = taxaCrescimentoAnual * 5 + populacaoMundial;
 
-        System.out.printf("População mundial estimada depois de um ano: %d%n bi", ano1);
-        System.out.printf("População mundial estimada depois de dois anos: %d%n bi", ano2);
-        System.out.printf("População mundial estimada depois de três anos: %d%n bi", ano3);
-        System.out.printf("População mundial estimada depois de quatro anos: %d%n bi", ano1);
-        System.out.printf("População mundial estimada depois de cinco anos: %d%n bi", ano1);
+        System.out.printf("População mundial estimada depois de um ano: %d bi", ano1);
+        System.out.printf("População mundial estimada depois de dois anos: %d bi", ano2);
+        System.out.printf("População mundial estimada depois de três anos: %d bi", ano3);
+        System.out.printf("População mundial estimada depois de quatro anos: %d bi", ano1);
+        System.out.printf("População mundial estimada depois de cinco anos: %d bi", ano1);
     }
 
 }
