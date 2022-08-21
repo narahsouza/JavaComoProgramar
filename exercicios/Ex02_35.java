db1d241a191a5d4a2d226238a03e57e178b05550
narahsouza <narahnsa@gmail.com>
2022-08-21 15:06:57 -0300

    Update

--- exercicios/Ex02_35.java
+++ exercicios/Ex02_35.java
@@ -2,5 +2,15 @@
 // Calculadora de economia da faixa solidária
 
 public class Ex02_35 {
+
+    public static void main(String [] args) {
+
+        int quilometrosDia; // a) Quilômetros totais dirigidos por dia.
+        int preco; // b) Preço por litro de gasolina.
+        int qulometrosLitro; // c) Quilômetros médios por litro.
+        int taxas; //d) Taxas de estacionamento por dia.
+        int pedagio; // e) Pedágio por dia.
+        
+    }
     
 }
