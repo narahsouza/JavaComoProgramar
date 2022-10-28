800fc0f6ab37665ba6fdd25c68f9789ecb15d34d
narahsouza <narahnsa@gmail.com>
2022-10-28 14:37:16 -0300

    Update

--- exercicios/Ex03_01.java
+++ exercicios/Ex03_01.java
@@ -1,27 +1,36 @@
 // Exercício 3.01: Ex03_01.java
 // Addition program that displays the sum of two numbers.
-import java.util.Scanner; // program uses class Scanner
+import javax.swing.JOptionPane;
 
-public class Addition 
+public class Ex03_01 
 {
    // main method begins execution of Java application
    public static void main(String[] args)
    {
-      // create a Scanner to obtain input from the command window
-      Scanner input = new Scanner(System.in);
-
+      String entrada1; // first number to enter
+      String entrada2; // second number to enter
       int number1; // first number to add
       int number2; // second number to add
       int sum; // sum of number1 and number2
+      String message;
 
-      System.out.print("Enter first integer: "); // prompt 
-      number1 = input.nextInt(); // read first number from user
+      // prompt user to enter number1
+      entrada1 = JOptionPane.showInputDialog("Enter first integer:");
+      number1 = Integer.parseInt(entrada1); // Converte entrada1 para inteiro
 
-      System.out.print("Enter second integer: "); // prompt 
-      number2 = input.nextInt(); // read second number from user
-
+      // prompt user to enter number2
+      entrada2 = JOptionPane.showInputDialog("Enter second integer:");
+      number2 = Integer.parseInt(entrada2); // Converte entrada2 para inteiro
+      
       sum = number1 + number2; // add numbers, then store total in sum
 
-      System.out.printf("Sum is %d%n", sum); // display sum
+      // create the message
+      message = String.format("Sum is %d%n", sum); 
+
+      // display the message to welcome the user by name 
+      JOptionPane.showMessageDialog(null, message);
+
    } // end method main
-} // end class Addition
+} // end class Ex03_01
+
+
